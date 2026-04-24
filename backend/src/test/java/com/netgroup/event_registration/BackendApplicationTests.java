3ae81129116c4c300029d5268762ceb858672475
dmitrikurtukov <kurtukov9@gmail.com>
2026-04-25 00:36:53 +0300

    chore: initialize backend and frontend projects

---
+++ backend/src/test/java/com/netgroup/event_registration/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netgroup.event_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
