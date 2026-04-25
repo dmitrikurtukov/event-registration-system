46e1744e447ea6e4979a2c50dffa44d83cb17629
dmitrikurtukov <kurtukov9@gmail.com>
2026-04-25 15:39:56 +0300

    feat(backend): implement security configuration for event API

--- backend/src/main/java/com/netgroup/event_registration/event/EventController.java
+++ backend/src/main/java/com/netgroup/event_registration/event/EventController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +22,7 @@ public class EventController {
     private final EventService eventService;
 
     @GetMapping
-    public List<EventResponse> getEvents(@RequestParam String param) {
+    public List<EventResponse> getEvents() {
         return eventService.getEvents();
     }
 
